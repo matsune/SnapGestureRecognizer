2b890c2fd016129038ba392aeaec6f160114724f
Yuma Matsune <yuma.matsune@gmail.com>
2018-01-23 15:11:16 +0900

    initial commit

---
+++ SnapGestureRecognizer/SnapGestureRecognizer.h
@@ -0,0 +1,19 @@
+//
+//  SnapGestureRecognizer.h
+//  SnapGestureRecognizer
+//
+//  Created by Yuma Matsune on 2018/01/23.
+//  Copyright © 2018年 matsune. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SnapGestureRecognizer.
+FOUNDATION_EXPORT double SnapGestureRecognizerVersionNumber;
+
+//! Project version string for SnapGestureRecognizer.
+FOUNDATION_EXPORT const unsigned char SnapGestureRecognizerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SnapGestureRecognizer/PublicHeader.h>
+
+
